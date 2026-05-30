13cea38ca5e75b23c8f200b4e60b536d4deb1b3f
msrirudhra <msrirudhra@gmail.com>
2026-05-31 00:16:49 +0530

    "Added collection.cs and updated other files".

---
+++ collection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class collections
+{
+    public void show()
+    {
+        //List<string> names = new List<string>();
+
+        //names.Add("rudhra");
+        //names.Add("ram");
+        //names.Add("kumar");
+        //names.Remove("bro");
+
+        //foreach (string name in names)
+        //{
+        //    //Console.WriteLine(name);
+        //    //Console.WriteLine(name.Count());
+
+        //    Console.WriteLine(names[2]);
+
+        //}
+
+    //    Dictionary<int, string> students = new Dictionary<int, string>();
+
+    //    students.Add(1, "jaa"); 
+    //    students.Add(2, "ram");
+    //    students.Add(3, "umar");
+
+    //    foreach(var student in students)
+    //    {
+    //        Console.WriteLine(student.Key +":" +  student.Value);
+    //    }
+    //}
+}
+
