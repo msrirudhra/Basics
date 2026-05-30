7bbeb4c18432a0053c3da2b107bef4496e03d066
msrirudhra <msrirudhra@gmail.com>
2026-05-31 01:04:22 +0530

    Update conditions.cs

--- conditions.cs
+++ conditions.cs
@@ -4,32 +4,32 @@ class condition
 {
     public void show()
     {
-        //int age = 20;
-        //bool hasIdCard = true;
-        //if (age >= 18)
-        //{
-        //    if (hasIdCard)
-        //    {
-        //        Console.WriteLine("Allowed");
-        //    }
-        //}
+        int age = 20;
+        bool hasIdCard = true;
+        if (age >= 18)
+        {
+           if (hasIdCard)
+           {
+               Console.WriteLine("Allowed");
+           }
+        }
 
 
-        //string username = "admin";
-        //string password = "1234";
-        //Console.WriteLine("Enter a username:");
-        //string username = Console.ReadLine();
-        //Console.WriteLine("Enter a password:");
-        //int password = Convert.ToInt32(Console.ReadLine());
+        string username = "admin";
+        string password = "1234";
+        Console.WriteLine("Enter a username:");
+        string username = Console.ReadLine();
+        Console.WriteLine("Enter a password:");
+        int password = Convert.ToInt32(Console.ReadLine());
 
-        //if (username == "admin" && password == 1234)
-        //{
-        //    Console.WriteLine("Login Success");
-        //}
-        //else
-        //{
-        //    Console.WriteLine("Invalid Login");
-        //}
+        if (username == "admin" && password == 1234)
+        {
+           Console.WriteLine("Login Success");
+        }
+        else
+        {
+           Console.WriteLine("Invalid Login");
+        }
 
 
 
