742c1d7bdeb0aa2115a1444cb498ae5c6d666de9
msrirudhra <msrirudhra@gmail.com>
2026-06-05 16:54:32 +0530

    updated

---
+++ methodoverloading.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+class MathOperations
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+}
